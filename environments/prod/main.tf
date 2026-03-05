46614018dd929a876ac320e3e4dd3bdb3ae84303
Amir Czwink <amir130@hotmail.de>
2026-03-05 22:24:23 +0100

    go

--- environments/prod/main.tf
+++ environments/prod/main.tf
@@ -8,3 +8,12 @@ module "platform" {
     environment = local.env
     location = var.location
 }
+
+module "app_kungfudoc" {
+    source = "../../modules/app-kungfudoc"
+
+    appServicePlanId = module.platform.appServicePlanId
+    environment = local.env
+    location = var.location
+    logAnalyticsWorkspaceId = module.platform.logAnalyticsWorkspaceId
+}
