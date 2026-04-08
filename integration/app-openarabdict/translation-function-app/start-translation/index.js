99cfe5a2693e2fad1843725cf72afc3b7cf8174d
Amir Czwink <amir130@hotmail.de>
2026-04-08 12:06:01 +0200

    fix

--- integration/app-openarabdict/translation-function-app/start-translation/index.js
+++ integration/app-openarabdict/translation-function-app/start-translation/index.js
@@ -3,8 +3,8 @@ const df = require("durable-functions");
 module.exports = async function (context, req)
 {
     const client = df.getClient(context);
+    
     const instanceId = "translation-instance";
-
     const existing = await client.getStatus(instanceId);
 
     context.log(existing);
